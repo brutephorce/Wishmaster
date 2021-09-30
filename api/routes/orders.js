7ba824b06bdb9b72711a91b88f71753c1c2aa0c8
Ravi Prakash Mishra <85183828+brutephorce@users.noreply.github.com>
2021-09-30 22:42:30 +0530

    Add files via upload

---
+++ api/routes/orders.js
@@ -0,0 +1,90 @@
+
+
+
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+
+
+
+router.get('/',(req,res,next) => {
+    Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map(doc =>{
+                return {
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + doc._id
+                    }
+
+                }
+            })
+            
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+13:57 se shuru krna h
+
+
+router.post('/',(req,res,next) => {
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+    });
+    order
+        .save()
+        .then( result => {
+            res.status(201).json({
+                message: 'Order stored',
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' + result._id
+
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    
+});
+
+router.get('/:orderId',(req,res,next) => {
+    res.status(200).json({
+        message : 'Order details',
+        orderId : req.params.orderId
+    });
+});
+
+router.delete('/:orderId',(req,res,next) => {
+    res.status(200).json({
+        message : 'Order deleted',
+        orderId : req.params.orderId
+    });
+});
+
+module.exports = router;
